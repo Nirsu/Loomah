c724716f6023cca9208beda3b59efad427794824
Nirsu <meru.fontaine@gmail.com>
2026-02-13 17:26:51 +0100

    [FEAT]: Update dependencies and enhance project configuration with Marionette integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loomah/env/env.dart';
 import 'package:loomah/features/map/presentation/widgets/custom_map_widget.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:marionette_flutter/marionette_flutter.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  if (kDebugMode) {
+    MarionetteBinding.ensureInitialized();
+  } else {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
 
   MapboxOptions.setAccessToken(Env.mapboxAccessToken);
 
